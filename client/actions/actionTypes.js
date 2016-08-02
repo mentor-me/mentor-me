393062edb74d44f9cca25623113adb3232c2a3bf
chaddbennett <chaddbennett@users.noreply.github.com>
2016-08-01 19:00:26 -0700

    [client] intial calendar component and action layout

--- client/actions/actionTypes.js
+++ client/actions/actionTypes.js
@@ -4,3 +4,7 @@ export const UNAUTH_USER = 'unauth_user';
 
 /* API/LEARNER */
 export const MENTORS = 'mentors';
+
+/* API/APPT */
+export const CREATE_APPOINTMENT = 'create_appoinment';
+export const FETCH_APPOINTMENTS = 'fetch_appointments';
