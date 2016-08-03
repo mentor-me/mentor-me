5744df453f2345bb5ff6822e0f51e48602fc2940
chaddbennett <chaddbennett@users.noreply.github.com>
2016-08-03 15:40:20 -0700

    [git] fixed app.css

--- client/actions/actionTypes.js
+++ client/actions/actionTypes.js
@@ -7,7 +7,6 @@ export const MENTORS = 'mentors';
 export const LEARNER_PREFERENCES = 'learner_preferences';
 export const CURRENT_MENTOR = 'current_mentor';
 
-
-/* API/APPT */
+/* API/APPOINTMENTS */
 export const CREATE_APPOINTMENT = 'create_appoinment';
 export const FETCH_APPOINTMENTS = 'fetch_appointments';
