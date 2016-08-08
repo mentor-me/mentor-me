3187d03bbcb6bfb516d8211d9b72193c31beb351
chaddbennett <chadd.d.bennett@gmail.com>
2016-08-07 19:05:42 -0700

    [heroku] adjusted server file and proc and config 5

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,28 +1,46 @@
+const path = require('path')
+const webpack = require('webpack')
+
 module.exports = {
+  devtool: 'source-map',
+
   entry: [
-    './client/index.js'
+      './client/index.js'
   ],
+
   output: {
-    path: __dirname,
-    publicPath: '/',
+    path: path.join(__dirname, 'client'),
     filename: 'bundle.js',
-    //dev only
-    headers: { 'Access-Control-Allow-Origin': '*' }
+    publicPath: './'
   },
-  module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
+
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
       }
-    }]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
+
+  module: {
+    loaders: [
+      { test: /\.js?$/,
+        loader: 'babel',
+        include: path.join(__dirname, 'client') },
+      { test: /\.scss?$/,
+        loader: 'style!css!sass',
+        include: path.join(__dirname, 'client', 'assets', 'sass') },
+      { test: /\.png$/,
+        loader: 'file' },
+      { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        loader: 'file'}
+    ]
   }
-};
+}
