27e9053d33fdb0c5377db24c31a6b12a6977900e
chaddbennett <chadd.d.bennett@gmail.com>
2016-08-07 18:46:45 -0700

    [heroku] adjusted server file and proc and config 2

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,42 +1,28 @@
-const path = require('path')
-const webpack = require('webpack')
-
 module.exports = {
-  devtool: 'source-map',
-
   entry: [
-      './client/index.js'
+    './client/index.js'
   ],
-
   output: {
-    path: path.join(__dirname, 'client'),
+    path: __dirname,
+    publicPath: '/',
     filename: 'bundle.js',
-    publicPath: '/client/'
+    //dev only
+    headers: { 'Access-Control-Allow-Origin': '*' }
   },
-
-  plugins: [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    })
-  ],
-
   module: {
-    loaders: [
-      { test: /\.js?$/,
-        loader: 'babel',
-        include: path.join(__dirname, 'client') },
-      { test: /\.scss?$/,
-        loader: 'style!css!sass',
-        include: path.join(__dirname, 'client', 'assets', 'sass') }
-    ]
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
   }
-}
+};
